77567f341b04f6cfd9403cdcc9065f9e4089061e
varalakshmidgb <you@example.comlakshmidgb@gmail.com>
2022-12-11 16:42:13 +0000

    added relational fields,modified views,added menu and added security

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -24,6 +24,9 @@
     'data': [
         'security/academy_security.xml',
         'security/ir.model.access.csv',
+        'views/academy_menuitems.xml',
+        'views/course_views.xml',
+        'views/session_views.xml',
     ],
     'demo': 
     ['demo/academy_demo.xml',        
