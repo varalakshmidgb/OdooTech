a1680c89fb3f7f9b6dd438f9321c7eec68a881d3
varalakshmidgb <lakshmidgb@gmail.com>
2022-12-15 13:15:57 +0000

    wizard,controllers,report files added,security updated

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -18,8 +18,8 @@
     
     'category': 'Training',
     'version': '0.1',
-    
-    'depends': ['base'],
+    #change name of sale app in v15
+    'depends': ['sale_management','website'],
     
     'data': [
         'security/academy_security.xml',
@@ -27,6 +27,11 @@
         'views/academy_menuitems.xml',
         'views/course_views.xml',
         'views/session_views.xml',
+        'views/sales_views_inherit.xml',
+        'views/product_views_inherit.xml',
+        'views/academy_web_templates.xml',
+        'wizard/sale_wizard_view.xml',
+        'report/session_report_templates.xml',
     ],
     'demo': [
         'demo/academy_demo.xml',
